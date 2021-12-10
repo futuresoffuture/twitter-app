4fc1f6c542532d8b2934fb4ebf4df3e6f24a788d
Prabu <>
2021-12-10 10:05:43 -0500

    Enabled Cors

--- src/main/java/com/example/twitterapp/TwitterApplication.java
+++ src/main/java/com/example/twitterapp/TwitterApplication.java
@@ -3,6 +3,8 @@ package com.example.twitterapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example")
@@ -10,6 +12,16 @@ public class TwitterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TwitterApplication.class, args);
+	}
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/twitter-app/*")
+						.allowedOrigins("*");
+			}
+		};
 	}
 
 }
